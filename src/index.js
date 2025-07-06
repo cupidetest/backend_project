464ab3734c53f07b7fa6ff952be3f5df88625908
cupidetest <snehagupta0206@gmail.com>
2025-07-06 23:35:23 +0530

    utils basics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`server listening on port ${process.env.PORT}`);
+});
+})
+.catch((error)=>{
+    console.log("mongodb not connected.")
+});
+
 
 
 /*
